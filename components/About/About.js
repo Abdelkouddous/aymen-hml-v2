3ff7cb8151866f9549900bef3ed3e976b7ef1fb8
Abdelkouddous <58855794+Abdelkouddous@users.noreply.github.com>
2024-07-10 22:45:33 +0100

    Fixed the project files

--- components/About/About.js
+++ components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/about2.png";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -13,7 +13,13 @@ function About() {
       <Container fluid className="about-section">
         <Particle />
         <Container>
-          <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Row
+            style={{
+              justifyContent: "center",
+              padding: "10px",
+              verticalAlign: "center",
+            }}
+          >
             <Col
               md={7}
               style={{
